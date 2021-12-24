d1411a6fed16de23a259bd281f9b77be426a1c02
DavidWeinstein <weinstein.dev@gmail.com>
2021-12-23 19:08:08 -0500

    working on different views, not complete

--- scrap_it/price_list/views.py
+++ scrap_it/price_list/views.py
@@ -1,3 +1,12 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Materials
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello world, we are at the Scrap It index page.")
+
+def detail(request, materials_id):
+    return HttpResponse("The material is %s." % Materials.material_name)
